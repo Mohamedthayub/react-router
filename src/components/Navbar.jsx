d37914db2430b2134378a67904ecbfabc4495c74
buyath <buyaththayub@gmail.com>
2025-09-05 19:52:31 +0000

    link added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Logo from "../images/logo.svg";
+import {Link} from "react-router-dom";
 const Navbar = () => {
     return (
         <div className="navbar">
             <img src={Logo}></img>
             <ul>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Home</li>
-                <li>Product</li>
+               <Link to={"/"}><li>Home</li></Link> 
+               <Link to={"/contact"}><li>Contact</li></Link>
+               <Link to={"/about"}><li>About</li></Link> 
+               <Link to={"/product"}><li>Product</li></Link>
             </ul>
             <button>Get Started</button>
         </div>
