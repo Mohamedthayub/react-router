d0e048492d672fd2106650ecf8abcc265a332b28
buyath <buyaththayub@gmail.com>
2025-09-05 16:59:04 +0000

    i have done the basic routing by react-router

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Logo from "../images/logo.svg";
+const Navbar = () => {
+    return (
+        <div className="Header">
+            <img src={Logo}></img>
+            <ul>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Home</li>
+                <li>Product</li>
+            </ul>
+            <button>Get Started</button>
+        </div>
+    )
+}
+
+export default Navbar;
